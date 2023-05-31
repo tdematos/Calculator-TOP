c82862e465c13a008fb1930b6998bde01a25b728
Thiago de Matos <thiago.dematos.1@gmail.com>
2023-05-30 23:05:56 -0400

    Started to add an HTML Structure and CSS Display

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-function add() {}
-function subtract() {}
-function multiply() {}
-function divide() {}
+const firstNum = 15;
+const secNum = 2;
+
+function add(a, b) {
+  return a + b;
+}
+add(firstNum, secNum);
+
+function subtract(a, b) {
+  return a - b;
+}
+subtract(firstNum, secNum);
+
+function multiply(a, b) {
+  return a * b;
+}
+multiply(firstNum, secNum);
+
+function divide(a, b) {
+  return a / b;
+}
+divide(firstNum, secNum);
+
+let operate = function (a, b) {
+  return multiply(a, b);
+};
+operate(firstNum, secNum);
