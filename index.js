c46936bd722839c687b6dd04ca5663d2fdca69fb
Thiago de Matos <thiago.dematos.1@gmail.com>
2023-12-19 17:40:54 -0500

    adding additional styling to buttons

--- index.js
+++ index.js
@@ -1,107 +1,50 @@
 //variables and operator for storing numbers
-const firstNum = 15;
-const secNum = 2;
-const opperators = ["+", "-", "*", "/"];
+const op1 = "";
+const op2 = "";
+const opperators = ["add", "subtract", "multiply", "divide"];
 //calculation operation functions
 const add = (a, b) => {
   return a + b;
 };
-add(firstNum, secNum);
+add(op1, op2);
 
 const subtract = (a, b) => {
   return a - b;
 };
-subtract(firstNum, secNum);
+subtract(op1, op2);
 
 const multiply = (a, b) => {
   return a * b;
 };
-multiply(firstNum, secNum);
+multiply(op1, op2);
 
 const divide = (a, b) => {
   return a / b;
 };
-divide(firstNum, secNum);
+divide(op1, op2);
 
 //function to execute calculations
-let operate = function (a, b) {
-  return multiply(a, b);
+let operate = (a, b, operand) => {
+  return operand(a, b);
 };
-operate(firstNum, secNum);
-//function for displaying value on calculator
 
-const zerobtn = document.querySelector(".zero");
-const onebtn = document.querySelector(".one");
-const twobtn = document.querySelector(".two");
-const threebtn = document.querySelector(".three");
-const fourbtn = document.querySelector(".four");
-const fivebtn = document.querySelector(".five");
-const sixbtn = document.querySelector(".six");
-const sevenbtn = document.querySelector(".seven");
-const eightbtn = document.querySelector(".eight");
-const ninebtn = document.querySelector(".nine");
-
-const displayScreen = document.querySelector(".calc-display");
-const pTag = document.createElement("p");
-const allBtns = document.querySelectorAll("button");
+let displayScreen = document.querySelector(".calc-display");
+let numDisplay = document.querySelector(".calc-numbers");
+let allBtns = document.querySelectorAll("button");
 
 allBtns.forEach(function (button) {
-  button.addEventListener("click", () => {
-    if (button === onebtn) {
-      const one = (pTag.textContent += "1");
-      displayScreen.appendChild(pTag);
-      console.log(one);
-    }
-
-    if (button === twobtn) {
-      const two = (pTag.textContent += "2");
-      displayScreen.appendChild(pTag);
-      console.log(two);
-    }
-
-    if (button === threebtn) {
-      const three = (pTag.textContent += "3");
-      displayScreen.appendChild(pTag);
-      console.log(three);
-    }
-
-    if (button === fourbtn) {
-      pTag.textContent += "4";
-      displayScreen.appendChild(pTag);
-    }
-
-    if (button === fivebtn) {
-      pTag.textContent += "5";
-      displayScreen.appendChild(pTag);
-    }
-
-    if (button === sixbtn) {
-      pTag.textContent += "6";
-      displayScreen.appendChild(pTag);
-    }
-
-    if (button === sevenbtn) {
-      pTag.textContent += "7";
-      displayScreen.appendChild(pTag);
-    }
-
-    if (button === eightbtn) {
-      pTag.textContent += "8";
-      displayScreen.appendChild(pTag);
-    }
-
-    if (button === ninebtn) {
-      pTag.textContent += "9";
-      displayScreen.appendChild(pTag);
-    }
-
-    if (button === zerobtn) {
-      pTag.textContent += "0";
-      displayScreen.appendChild(pTag);
-    }
+  button.addEventListener("click", (e) => {
+    updateDisplay(e.target.textContent);
   });
 });
 
-//create a variable to select all the button
-//created a command for itterating through all of the buttons
-//once a button is pressed,
+// function clearScreen() {
+//   numDisplay.value  = "";
+// }
+
+function updateDisplay(number) {
+  if (numDisplay.textContent.length <= 10) {
+    numDisplay.textContent += number;
+  } else {
+  }
+}
