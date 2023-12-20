8262c02ceddd752a6be72036ff244142fe07e526
Thiago de Matos <thiago.dematos.1@gmail.com>
2023-12-19 19:01:09 -0500

    creating function for selecting operator

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //variables and operator for storing numbers
 let currentvalue;
-const opperators = ["add", "subtract", "multiply", "divide"];
 
 //calculation operation functions
 const add = (a, b) => {
@@ -20,29 +19,57 @@ const divide = (a, b) => {
 };
 
 //function to display current value on screen
-function displayCurrentValue() {
-  let numDisplay = document.querySelector(".calc-numbers");
-  let allBtns = document.querySelectorAll("button");
+const displayCurrentValue = () => {
+  const numDisplay = document.querySelector(".calc-numbers");
+  const numberButtons = document.querySelectorAll(".num-btn");
+  const numBtns = Array.from(numberButtons);
 
-  allBtns.forEach(function (button) {
+  numBtns.forEach(function (button) {
     button.addEventListener("click", (e) => {
       numDisplay.textContent += e.target.textContent;
       let currentValue = numDisplay.innerText;
       console.log(currentValue);
+      return currentValue;
     });
   });
-}
+};
+
+//function for selecting the operation
+const selectOperation = () => {
+  const functionButtons = document.querySelectorAll(".func");
+  const funcBtns = Array.from(functionButtons);
+
+  funcBtns.forEach((Btn) => {
+    Btn.addEventListener("click", (e) => {
+      let operator = e.target.textContent;
+      console.log(operator);
+      return operator;
+    });
+  });
+};
 
 // function to calculate numbers
+const calculateNumbers = () => {
+  const numDisplay = document.querySelector(".calc-numbers");
+  displayCurrentValue();
+  selectOperation();
+
+  if (selectOperation() === "+") {
+    let valueA = displayCurrentValue();
+    numDisplay.textContent = "";
+  }
+};
 
 // function clearScreen() {
 
 // Function for initializing the calculator
 const initialize = () => {
-  displayCurrentValue();
+  // selectOperation();
+  calculateNumbers();
 };
 
 document.addEventListener("DOMContentLoaded", initialize);
 
 //Make sure can only type in numbers and not symbols
 //Make sure period can only be typed in once.
+//Add parameter for only being able to type in 10 numbers on the screen so no overflow happens
